880dc84e5c2e7de54585726d8161a248468a8387
larysalozynska <llozy@softserveinc.com>
2025-11-17 19:36:29 +0000

    Bull-45 BE fix sonarqube

--- auth/health.py
+++ auth/health.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 
-def health(_):
+@require_http_methods(["GET"])
+def health(request):
     """Health check view that returns 'OK'."""
     return HttpResponse("OK", status=200)
